1f559b06b7433850b1d875d53b8932389de58672
Giovanni L <callmegiorgio@hotmail.com>
2021-09-26 23:29:29 -0300

    updated sample 02 to track more symbols

--- python/sample/02_tick_received_loop.py
+++ python/sample/02_tick_received_loop.py
@@ -7,8 +7,11 @@ logging.getLogger().setLevel(logging.INFO)
 
 exchange = rmt.exchanges.MetaTrader4()
 exchange.subscribe('US100')
+exchange.subscribe('XAUUSD')
+exchange.subscribe('EURUSD')
+
 exchange.tick_received.connect(lambda symbol, tick: print(symbol, tick))
 
-for i in range(5):
+for i in range(10):
     exchange.process_events()
     sleep(1)
