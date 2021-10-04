82a2caf906625988415994fe77f1b91d7f0f92e8
Giovanni L <callmegiorgio@hotmail.com>
2021-10-04 10:36:11 -0300

    updated sample 09

--- python/sample/09_get_history_bars.py
+++ python/sample/09_get_history_bars.py
@@ -7,5 +7,10 @@ logging.getLogger().setLevel(logging.INFO)
 
 exchange = rmt.exchanges.MetaTrader4()
 
-for bar in exchange.get_bars('US100'):
+print('M1 bars:')
+for bar in exchange.get_history_bars('US100'):
+    print(bar)
+
+print('H1 bars:')
+for bar in exchange.get_history_bars('US100', timeframe=rmt.Timeframe.H1):
     print(bar)
