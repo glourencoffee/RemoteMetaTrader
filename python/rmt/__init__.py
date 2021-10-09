2d38204048fcc3b623931dfbdbdbf53361094906
Giovanni L <callmegiorgio@hotmail.com>
2021-10-08 21:23:23 -0300

    implemented functions read_ticks_from_csv() and read_bars_from_csv()

--- python/rmt/__init__.py
+++ python/rmt/__init__.py
@@ -2,11 +2,11 @@ from .slotted_class import SlottedClass
 from .              import error
 from .              import jsonutil
 from .account       import Account
-from .tick          import Tick
+from .tick          import Tick, read_ticks_from_csv
 from .instrument    import Instrument
 from .order         import Side, OrderType, OrderStatus, Order
 from .performance   import Performance
-from .bar           import Bar
+from .bar           import Bar, read_bars_from_csv
 from .timeframe     import Timeframe
 from .exchange      import Exchange
 from .strategy      import Strategy
--- python/rmt/tick.py
+++ python/rmt/tick.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timezone
-from typing   import Tuple
+from typing   import Tuple, List
 from rmt      import SlottedClass
 
 class Tick(SlottedClass):
@@ -9,12 +9,12 @@ class Tick(SlottedClass):
 
     def __init__(self,
                  server_time: datetime = datetime.fromtimestamp(0, timezone.utc),
-                 bid: float = float(0),
-                 ask: float = float(0)
+                 bid: float = 0,
+                 ask: float = 0
     ):
         self._server_time = server_time
-        self._bid         = bid
-        self._ask         = ask
+        self._bid         = float(bid)
+        self._ask         = float(ask)
 
     @property
     def server_time(self) -> datetime:
@@ -33,3 +33,25 @@ class Tick(SlottedClass):
 
     def __str__(self) -> str:
         return 'Tick({}, bid: {}, ask: {})'.format(self.server_time, self.bid, self.ask)
+
+def read_ticks_from_csv(filepath:   str,
+                        separator:  str = ',',
+                        time_index: int = 0,
+                        bid_index:  int = 1,
+                        ask_index:  int = 2
+) -> List[Tick]:
+    ticks = []
+
+    with open(filepath, 'r') as file:
+        for line in file:
+            values = line.split(separator)
+
+            tick = Tick(
+                server_time = datetime.fromtimestamp(int(values[time_index]), timezone.utc),
+                bid         = float(values[bid_index]),
+                ask         = float(values[ask_index])
+            )
+
+            ticks.append(tick)
+
+    return ticks
