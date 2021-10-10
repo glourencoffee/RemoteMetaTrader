484ec6d0f4532b0f798357766ccdb95a201d76c5
Giovanni L <callmegiorgio@hotmail.com>
2021-10-10 11:20:27 -0300

    added version and updated setup.py

--- MQL4/Experts/RMTExpert.mq4
+++ MQL4/Experts/RMTExpert.mq4
@@ -1,3 +1,4 @@
+#property version "0.1.0"
 #property strict
 
 #include "../Include/RMT/Command/RequestProcessor.mqh"
---
+++ python/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rmt',
+    version='0.1.0',
+    packages=find_packages(include=['rmt'])
+)
