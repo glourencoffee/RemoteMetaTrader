0dd91c8444bf5e9068a03181be94a69d5d56c0e3
Giovanni L <callmegiorgio@hotmail.com>
2021-10-24 20:04:16 -0300

    minor changes to class Dispatcher in module 'events'

--- python/rmt/exchanges/mt4/events/dispatcher.py
+++ python/rmt/exchanges/mt4/events/dispatcher.py
@@ -8,8 +8,11 @@ class Dispatcher:
     def __init__(self):
         self._event_data: Dict[str, Tuple[Event, Content]] = {}
 
-    def register(self, event_name: str, event_type: Type[Event], handler: Callable[[Event], None]):
-        self._event_data[event_name] = (event_type, handler)
+    def register(self, static_name: str, event_type: Type[Event], handler: Callable[[Event], None]):
+        if not static_name.isalpha():
+            raise ValueError("expected alphabetic static part of event name (got: '{}')".format(static_name))
+
+        self._event_data[static_name] = (event_type, handler)
 
     def dispatch(self, event_name: str, content: Content):
         static_name  = None
@@ -19,12 +22,15 @@ class Dispatcher:
         if dynamic_name_index != -1:
             static_name  = event_name[0:dynamic_name_index]
             dynamic_name = event_name[(dynamic_name_index + 1):]
+
+            if dynamic_name == '':
+                raise ValueError(
+                    "received event message with static name '{}' and empty dynamic name"
+                    .format(static_name)
+                )
         else:
             static_name = event_name
             dynamic_name = ''
-
-        if not static_name.isalpha():
-            raise ValueError("expected alphabetic static part of event name (got: '%s')" % static_name)
 
         if static_name not in self._event_data:
             raise ValueError("received event message with unknown name '%s'" % static_name)
