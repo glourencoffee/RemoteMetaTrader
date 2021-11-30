e50f6aaf935cddab6747ace1985b94f56b3ea011
Giovanni L <callmegiorgio@hotmail.com>
2021-11-30 15:24:53 -0300

    log orders placed by RiskManagedStrategy.place_order_on_risk()

--- python/rmt/strategies/risk_managed_strategy.py
+++ python/rmt/strategies/risk_managed_strategy.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from typing   import Optional
-from rmt      import Strategy, Exchange, Side, OrderType, error
+from rmt      import Strategy, Exchange, Side, Order, OrderType, error
 
 class RiskCalculator:
     def calculate(self, balance: float) -> float:
@@ -18,6 +18,9 @@ class PercentRiskCalculator(RiskCalculator):
     def calculate(self, balance: float) -> float:
         return balance * self._percent_ratio
 
+    def __str__(self) -> str:
+        return 'PercentRiskCalculator(percent={}%)'.format(self._percent_ratio * 100)
+
 class FixedRiskCalculator(RiskCalculator):
     def __init__(self, amount: float) -> None:
         super().__init__()
@@ -29,6 +32,9 @@ class FixedRiskCalculator(RiskCalculator):
     
     def calculate(self, balance: float) -> float:
         return self._amount
+
+    def __str__(self) -> str:
+        return 'FixedRiskCalculator(amount={})'.format(self._amount)
 
 class RiskManagedStrategy(Strategy):
     """Implements a risk-managed strategy."""
@@ -56,34 +62,15 @@ class RiskManagedStrategy(Strategy):
         self._risk_calculator = calculator
 
     def risk_price(self) -> float:
+        """Returns the risk price, in account currency, calculated by this strategy's risk calculator."""
+
         balance = min(max(self.available_balance(), 0), self.account.balance)
         balance = float(balance)
 
         risk_price = self._risk_calculator.calculate(balance)
 
-        if self.instrument.quote_currency != self.account.currency:
-            # Note: if `Exchange` is `MetaTrader4`, and the Expert Server is run by
-            # Strategy Tester, the call below fails. MT4 doesn't support multi-currency
-            # testing, so we can't exchange rates here, and thus this method is unable
-            # to calculate the risk price in the instrument's quote currency.
-            # 
-            # A workaround, for the time being, is to comment the below call and hard-code
-            # the exchange rate yourself. For instance, if `self.instrument` is US100
-            # (in which case `self.instrument.quote_currency` is USD) and the account
-            # currency is EUR, you must set the bid price of EURUSD yourself:
-            #
-            # rate = EURUSD bid price
-            #
-            # Argh, if only MT4 made our testing lives easier...
-
-            rate = self.get_exchange_rate(
-                self.account.currency,
-                self.instrument.quote_currency
-            )
-
-            risk_price *= rate
-
-        return self.instrument.normalize_price(risk_price)
+        # TODO: don't assume account currency has 2 digits
+        return round(risk_price, 2)
 
     def place_order_on_risk(self,
                             side:         Side,
@@ -95,17 +82,27 @@ class RiskManagedStrategy(Strategy):
                             comment:      str = '',
                             magic_number: int = 0,
                             expiration:   Optional[datetime] = None
-    ) -> int:
-        price     = float(price)
-        stop_loss = float(stop_loss)
+    ) -> Order:
+        open_price     = float(price)
+        stop_loss      = float(stop_loss)
+        max_risk_price = self.risk_price()
 
-        lots = self._calculate_lots(price, stop_loss)
+        exchange_rate = 1.0
 
-        ticket = self.place_order(
+        if self.instrument.quote_currency != self.account.currency:
+            exchange_rate = self.get_exchange_rate(
+                self.account.currency,
+                self.instrument.quote_currency
+            )
+
+        max_risk_price = self.instrument.normalize_price(max_risk_price * exchange_rate)
+        lots           = self._calculate_lots(max_risk_price, open_price, stop_loss)
+
+        order = self.place_order(
             side         = side,
             order_type   = order_type,
             lots         = lots,
-            price        = price,
+            price        = open_price,
             slippage     = slippage,
             stop_loss    = stop_loss,
             take_profit  = take_profit,
@@ -115,12 +112,41 @@ class RiskManagedStrategy(Strategy):
             symbol       = self.instrument.symbol
         )
 
-        return ticket
+        # The calculated risk may result in a lot size greater than `self.instrument.max_lot`,
+        # in which case the lot size is clamped down to the maximum lot. However, that means
+        # we haven't placed an order at `max_risk_price`, so the actual risk price must be
+        # recalculated. For example, say the account balance is $1,000,000 and risk price is
+        # 10% of that. The calculated lot for $100,000 may result in a lot size of 1000 or
+        # so, but some instruments only support up to 500 lots, so that won't be possible.
+        # `actual_risk_price` will calculate the actual amount of money which was put at risk.
+        actual_risk_price = self.instrument.normalize_price(abs(order.open_price - order.stop_loss) * lots)
+        
+        if self.instrument.quote_currency == self.account.currency:
+            # If instrument's quote currency is same as account currency, print risk price as-is.
+            actual_risk_price_str = '{} {}'.format(actual_risk_price, self.account.currency)
+        else:
+            # Example: for instrument HK50 with an account currency of USD, this will print,
+            # "100 USD (778 HKD)".
+            actual_risk_price_str = '{} {} ({} {})'.format(
+                round(actual_risk_price / exchange_rate, 2), # TODO: don't assume 2 decimal places
+                self.account.currency,
+                actual_risk_price,
+                self.instrument.quote_currency
+            )
 
-    def _calculate_lots(self, price: float, stop_loss: float) -> float:
-        risk_price = self.risk_price()
+        self.logger.info(
+            'placed %s %s at price %f with risk price of %s on risk calculator %s',
+            side.name.lower(),
+            order_type.name.lower().replace('_', ' '),
+            order.open_price,
+            actual_risk_price_str,
+            self.risk_calculator
+        )
 
-        price_distance = self.instrument.normalize_price(abs(price - stop_loss))
+        return order
+
+    def _calculate_lots(self, risk_price: float, open_price: float, stop_loss: float) -> float:
+        price_distance = self.instrument.normalize_price(abs(open_price - stop_loss))
         point_distance = int(price_distance / self.instrument.point)
 
         cost_of_1_lot = point_distance * self.instrument.point_value
